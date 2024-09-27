d95b3478e41391b9eec167a12019a00fd18fe44d
Grant McKenna <grant.mckenna@madetech.com>
2024-09-27 12:08:13 +0100

    Amend Program to convert Met Office hourly data to collection of strings

--- Program.cs
+++ Program.cs
@@ -4,12 +4,26 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 class Program()
 {
     static async Task Main(string[] args)
     {
         var hourlySpotData = await FetchDataFromApi("https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly");
+
+        var hourlySpotDataAsText = ExtractTextFromJson(hourlySpotData);
+
+        //Debugging purposes only!
+        // foreach(var text in hourlySpotDataAsText)
+        // {
+        //     Console.WriteLine($"Text: {text}");
+        // }
 
         Console.WriteLine("Finished!");
     }
@@ -31,9 +45,6 @@ class Program()
 
                 jsonResponse = await response.Content.ReadAsStringAsync();
             }
-
-            Console.WriteLine("Retrieved Data:");
-            Console.WriteLine(jsonResponse);
         }
         catch (Exception ex)
         {
@@ -52,5 +63,59 @@ class Program()
 
         return configuration[keyName]??"";
     }
+
+    private static List<string> ExtractTextFromJson(string jsonData)
+    {
+        var extractedTexts = new List<string>();
+
+        try
+        {
+            var featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(jsonData);
+
+            var properties = featureCollection.Features.First().Properties;
+
+            foreach (var timeSeries in properties.TimeSeries)
+            {
+                var localDateTime = DateTime.Parse(timeSeries.Time, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                extractedTexts.Add($"Maximum temperature {timeSeries.MaxScreenAirTemp} and minimum temperature {timeSeries.MinScreenAirTemp} at {localDateTime.ToLocalTime()}" );
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error extracting text from JSON: {ex.Message}");
+        }            
+        
+        return extractedTexts;
+    }
+}
+
+public class FeatureCollection
+{
+    [JsonProperty("features")]
+    public List<Feature> Features { get; set; } = new List<Feature>();
+}
+
+public class Feature
+{
+    [JsonProperty("properties")]
+    public Properties? Properties {get; set;}
+}
+
+public class Properties
+{
+    [JsonProperty("timeSeries")]
+    public List<TimeSeries> TimeSeries { get; set; } = new List<TimeSeries>();
+}
+
+public class TimeSeries
+{
+    [JsonProperty("time")]
+    public string? Time { get; set; }
+
+    [JsonProperty("maxScreenAirTemp")]
+    public double MaxScreenAirTemp { get; set; }
+
+    [JsonProperty("minScreenAirTemp")]
+    public double MinScreenAirTemp { get; set; }
 
 }
