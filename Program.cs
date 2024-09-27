22bca40a08d2f1c25e26efb51991221e6dc4ce74
Grant McKenna <grant.mckenna@madetech.com>
2024-09-27 10:32:28 +0100

    Amend Program to fetch data for hard-code latitude and longitude

--- Program.cs
+++ Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 
 class Program()
 {
-    private static string _MetOfficeApiKey = String.Empty;
     static async Task Main(string[] args)
     {
-        GetConfigurationValues();
+        var retrievedData = await FetchDataFromApi("https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly");
 
         Console.WriteLine("Finished!");
     }
 
-    private static void GetConfigurationValues()
+     static async Task<string> FetchDataFromApi(string apiUrl)
+    {
+        var jsonResponse = String.Empty;
+
+        try
+        {
+            var requestUrl = $"{apiUrl}?latitude=51.652931&longitude=-0.199610";
+            var metOfficeApiKey = GetConfigurationValues("MetOfficeApiKey");
+
+            using (var client = new HttpClient()) {
+                client.DefaultRequestHeaders.Add("ApiKey", metOfficeApiKey);
+
+                var response = await client.GetAsync(requestUrl);
+                response.EnsureSuccessStatusCode();
+
+                jsonResponse = await response.Content.ReadAsStringAsync();
+            }
+
+            Console.WriteLine("Retrieved Data:");
+            Console.WriteLine(jsonResponse);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error retrieving data from API: {ex.Message}");
+        }
+
+        return jsonResponse;
+    }
+
+    
+    private static string GetConfigurationValues(string keyName)
     {
         var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
 
         IConfiguration configuration = builder.Build();
 
-        _MetOfficeApiKey = configuration["MetOfficeApiKey"]??"";
+        return configuration[keyName]??"";
     }
+
 }
